126d58bbdcefcd65647e580c22d6db5566e8f777
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-10 16:26:51 +0530

    ch_9

--- lib/main.dart
+++ lib/main.dart
@@ -1,148 +1,161 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-      padding: const EdgeInsets.symmetric(vertical: 50.0),
-      // padding: const EdgeInsets.all(50.0),
-      decoration: BoxDecoration(color: Colors.blueAccent),
-      child: Column(
+  runApp(Directionality(
+      textDirection: TextDirection.ltr,
+      child: ListView(
+        padding: const EdgeInsets.all(10.0),
         children: <Widget>[
-          Text(
-            "One",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+              height: 100,
+              color: Colors.purpleAccent,
+              child: const Center(
+                child: Text(
+                  "1",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(fontSize: 100),
+                ),
+              )),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "2",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Two",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "3",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Three",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "4",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Four",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "5",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Five",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "6",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Six",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "7",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Seven",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "8",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Eight",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "9",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
-          Text(
-            "Nine",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "10",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
+          ),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "11",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
+          ),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "12",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
+          ),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "13",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
+          ),
+          Container(
+            height: 100,
+            color: Colors.blueAccent,
+            child: const Center(
+                child: Text(
+              "14",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
+          ),
+          Container(
+            height: 100,
+            color: Colors.purpleAccent,
+            child: const Center(
+                child: Text(
+              "15",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 100),
+            )),
           ),
         ],
       )));
 }
-
-/*
-void main() {
-  runApp(Container(
-      padding: const EdgeInsets.symmetric(vertical: 50.0),
-      // padding: const EdgeInsets.all(50.0),
-      decoration: BoxDecoration(color: Colors.blueAccent),
-      child: Column(
-        children: <Widget>[
-          Expanded(
-              child: Text(
-            "1",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "2",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "3",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "4",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "5",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "6",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "7",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "8",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "9",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "10",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "11",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "12",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-          Expanded(
-              child: Text(
-            "13",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(fontSize: 50),
-          )),
-        ],
-      )));
-}
-*/
