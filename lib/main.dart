5d02102adfec86c6e650f8cf8ff2ea9a0f03f683
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-10 15:39:28 +0530

    ch_6

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Row(
-    textDirection: TextDirection.ltr,
-    children: [
-      Expanded(
-          child: Text(
+  runApp(Column(
+    children: <Widget>[
+      Text(
         "One",
         textDirection: TextDirection.ltr,
         style: TextStyle(fontSize: 50),
-      )),
-      Expanded(
-          child: Text(
+      ),
+      Text(
         "Two",
         textDirection: TextDirection.ltr,
         style: TextStyle(fontSize: 50),
-      )),
-      Expanded(
-          child: Text(
+      ),
+      Text(
         "Three",
         textDirection: TextDirection.ltr,
         style: TextStyle(fontSize: 50),
-      )),
-      Expanded(
-          child: Text(
+      ),
+      Text(
         "Four",
         textDirection: TextDirection.ltr,
         style: TextStyle(fontSize: 50),
-      )),
+      ),
     ],
   ));
 }
