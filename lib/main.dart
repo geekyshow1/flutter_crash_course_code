4924b3439ae0ba8573054f5bb184c6f4b5cec373
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-10 15:28:05 +0530

    ch_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-      child: Text(
-    "Hello Flutter",
+  runApp(Row(
     textDirection: TextDirection.ltr,
-    style: TextStyle(fontSize: 50),
-  )));
+    children: [
+      Text(
+        "One",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+      ),
+      Text(
+        "Two",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+      ),
+      Text(
+        "Three",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 50),
+      ),
+
+      // Below will show an error
+      // Text(
+      //   "Four",
+      //   textDirection: TextDirection.ltr,
+      //   style: TextStyle(fontSize: 50),
+      // ),
+    ],
+  ));
 }
