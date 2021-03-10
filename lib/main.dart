b5213a562880593e066926ce881f511070bb8664
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-10 16:00:35 +0530

    ch_8

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,148 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Column(
-    children: <Widget>[
-      Expanded(
-          child: Text(
-        "One",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 50),
-      )),
-      Expanded(
-          child: Text(
-        "Two",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 50),
-      )),
-      Expanded(
-          child: Text(
-        "Three",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 50),
-      )),
-      Expanded(
-          child: Text(
-        "Four",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 50),
-      )),
-    ],
-  ));
+  runApp(Container(
+      padding: const EdgeInsets.symmetric(vertical: 50.0),
+      // padding: const EdgeInsets.all(50.0),
+      decoration: BoxDecoration(color: Colors.blueAccent),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "One",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Two",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Three",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Four",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Five",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Six",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Seven",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Eight",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+          Text(
+            "Nine",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          ),
+        ],
+      )));
 }
+
+/*
+void main() {
+  runApp(Container(
+      padding: const EdgeInsets.symmetric(vertical: 50.0),
+      // padding: const EdgeInsets.all(50.0),
+      decoration: BoxDecoration(color: Colors.blueAccent),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+              child: Text(
+            "1",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "2",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "3",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "4",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "5",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "6",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "7",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "8",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "9",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "10",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "11",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "12",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+          Expanded(
+              child: Text(
+            "13",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 50),
+          )),
+        ],
+      )));
+}
+*/
