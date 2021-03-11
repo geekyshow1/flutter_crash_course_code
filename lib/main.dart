3dbf6d0dc2632996b0d8df87be490a17de20b42e
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-11 16:04:05 +0530

    ch_17

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,66 @@ void main() {
       title: 'Flutter',
       home: Scaffold(
           appBar: AppBar(
-            title: const Text(
-              'My Flutter App',
-              style: TextStyle(color: Colors.black),
-            ),
-            backgroundColor: Colors.cyanAccent,
+            title: const Text('My Flutter App'),
+            backgroundColor: Colors.blueAccent,
           ),
-          backgroundColor: Colors.cyan,
-          body: Stack(
+          body: ListView(
             children: [
-              Image(image: AssetImage('images/mobile/m1.jpg')),
-              Text('This is Image',
-                  style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.redAccent))
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 1'),
+                subtitle: Text('Secondary Text 1'),
+                trailing: Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m2.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+                trailing: Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+                trailing: Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m3.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+                trailing: Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+                trailing: Icon(Icons.delete),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+              ),
+              ListTile(
+                leading: Image.asset('images/mobile/m1.jpg'),
+                title: const Text('Card title 2'),
+                subtitle: Text('Secondary Text 2'),
+              ),
             ],
           ))));
 }
