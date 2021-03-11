849af989086cddb9b63ff73d116815c8400750b0
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-11 15:32:08 +0530

    ch_16

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,14 @@ void main() {
             backgroundColor: Colors.cyanAccent,
           ),
           backgroundColor: Colors.cyan,
-          body: GridView.count(
-            crossAxisCount: 2,
-            // mainAxisSpacing: 200,
-            // crossAxisSpacing: 100,
+          body: Stack(
             children: [
-              Image(image: AssetImage('images/tw4.jpg')),
-              Image(image: AssetImage('images/tw5.jpg')),
               Image(image: AssetImage('images/mobile/m1.jpg')),
-              Image(image: AssetImage('images/mobile/m2.jpg')),
-              Image(image: AssetImage('images/mobile/m3.jpg')),
-              Image(image: AssetImage('images/mobile/m4.jpg')),
-              Image(image: AssetImage('images/mobile/m5.jpg')),
-              Image(image: AssetImage('images/mobile/m6.jpg')),
-              Image(image: AssetImage('images/mobile/m7.jpg')),
+              Text('This is Image',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.redAccent))
             ],
           ))));
 }
