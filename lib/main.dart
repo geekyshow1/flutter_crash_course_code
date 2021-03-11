988c090d7ec5fd28051db6241ac64a4a10074196
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-11 13:25:07 +0530

    ch_11

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: 'Flutter',
-      home: Container(
-          decoration: BoxDecoration(color: Colors.blueAccent),
-          child: Center(
+      home: Scaffold(
+          backgroundColor: Colors.cyan,
+          body: Center(
               child: Text(
             "Material App",
             textDirection: TextDirection.ltr,
-            style:
-                TextStyle(color: Colors.white, decoration: TextDecoration.none),
+            style: TextStyle(
+                color: Colors.black,
+                decoration: TextDecoration.none,
+                fontSize: 50),
           )))));
 }
