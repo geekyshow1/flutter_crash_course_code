6b46ac94e938e6326a5bd584b24a8fdf7ee02c2e
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-11 16:16:02 +0530

    ch_18

--- lib/main.dart
+++ lib/main.dart
@@ -8,63 +8,23 @@ void main() {
             title: const Text('My Flutter App'),
             backgroundColor: Colors.blueAccent,
           ),
-          body: ListView(
-            children: [
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 1'),
-                subtitle: Text('Secondary Text 1'),
-                trailing: Icon(Icons.delete),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m2.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-                trailing: Icon(Icons.delete),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-                trailing: Icon(Icons.delete),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m3.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-                trailing: Icon(Icons.delete),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-                trailing: Icon(Icons.delete),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-              ),
-              ListTile(
-                leading: Image.asset('images/mobile/m1.jpg'),
-                title: const Text('Card title 2'),
-                subtitle: Text('Secondary Text 2'),
-              ),
-            ],
-          ))));
+          body: Center(
+              child: SizedBox(
+                  height: 350,
+                  child: Card(
+                      child: Padding(
+                          padding: EdgeInsets.all(30.0),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                'images/mobile/m1.jpg',
+                                height: 200,
+                              ),
+                              Divider(),
+                              Text(
+                                'Samsung A 10',
+                                style: TextStyle(fontSize: 30),
+                              ),
+                            ],
+                          ))))))));
 }
