df44ee3cea1b5c80c9d28990a0207143a3bf3307
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-12 11:50:23 +0530

    ch_19

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: 'Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('My Flutter App'),
-            backgroundColor: Colors.blueAccent,
-          ),
-          body: Center(
-              child: SizedBox(
-                  height: 350,
-                  child: Card(
-                      child: Padding(
-                          padding: EdgeInsets.all(30.0),
-                          child: Column(
-                            children: [
-                              Image.asset(
-                                'images/mobile/m1.jpg',
-                                height: 200,
-                              ),
-                              Divider(),
-                              Text(
-                                'Samsung A 10',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ],
-                          ))))))));
+  runApp(MaterialApp(title: 'Flutter', home: MyHome()));
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My Flutter App'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+            child: SizedBox(
+                height: 350,
+                child: Card(
+                    child: Padding(
+                        padding: EdgeInsets.all(30.0),
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              'images/mobile/m1.jpg',
+                              height: 200,
+                            ),
+                            Divider(),
+                            Text(
+                              'Samsung A 10',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ],
+                        ))))));
+  }
 }
