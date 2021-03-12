c8830207cd6cadee8bd67d5a0c77c00277b4860f
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-12 12:03:35 +0530

    ch_19

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: 'Flutter', home: MyHome()));
+  runApp(MyApp());
 }
 
-class MyHome extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  Widget build(Object context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('My Flutter App'),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Center(
-            child: SizedBox(
-                height: 350,
-                child: Card(
-                    child: Padding(
-                        padding: EdgeInsets.all(30.0),
-                        child: Column(
-                          children: [
-                            Image.asset(
-                              'images/mobile/m1.jpg',
-                              height: 200,
-                            ),
-                            Divider(),
-                            Text(
-                              'Samsung A 10',
-                              style: TextStyle(fontSize: 30),
-                            ),
-                          ],
-                        ))))));
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('My Flutter App'),
+              backgroundColor: Colors.blueAccent,
+            ),
+            body: Center(
+                child: SizedBox(
+                    height: 350,
+                    child: Card(
+                        child: Padding(
+                            padding: EdgeInsets.all(30.0),
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'images/mobile/m1.jpg',
+                                  height: 200,
+                                ),
+                                Divider(),
+                                Text(
+                                  'Samsung A 10',
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                              ],
+                            )))))));
   }
 }
