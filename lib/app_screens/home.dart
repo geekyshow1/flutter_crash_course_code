95c6d617f28aacd7cab99f0f10f6990a2b930aa7
Geeky Shows <32277105+geekyshow1@users.noreply.github.com>
2021-03-14 16:39:39 +0530

    ch_26

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,30 +6,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String name = "";
+  final _formKey = GlobalKey<FormState>();
+  var name = "";
+  var email = "";
+  var password = "";
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(children: [
-      TextField(
-        onSubmitted: (value) {
-          // name = value;
-          setState(() {
-            name = value;
-          });
-        },
-        // onChanged: (value) {
-        //   // name = value;
-        //   setState(() {
-        //     name = value;
-        //   });
-        // },
-      ),
-      Text(
-        'Name: $name',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30),
-      ),
-    ]));
+    return Form(
+        key: _formKey,
+        child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+            child: ListView(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'Name: ',
+                      labelStyle: TextStyle(fontSize: 20),
+                      errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15)),
+                  controller: nameController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please Enter Name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'Email: ',
+                      labelStyle: TextStyle(fontSize: 20),
+                      errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15)),
+                  controller: emailController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please Enter Email';
+                    } else if (!value.contains('@')) {
+                      return 'Please Enter Valid Email';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Password: ',
+                      labelStyle: TextStyle(fontSize: 20),
+                      errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15)),
+                  controller: passwordController,
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please Enter Password';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, otherwise false.
+                    if (_formKey.currentState.validate()) {
+                      setState(() {
+                        name = nameController.text;
+                        email = emailController.text;
+                        password = passwordController.text;
+                      });
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+                Text('Name: $name'),
+                Text('Email: $email'),
+                Text('Password: $password'),
+              ],
+            )));
   }
 }
